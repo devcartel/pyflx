d3d428a70da55a9014161cd1b71bd4224edd09e3
Wiwat Tharateeraparb <wiwat.t@devcartel.com>
2020-04-06 09:51:35 +0700

    Bump version to 1.3.0

--- pyflx/__init__.py
+++ pyflx/__init__.py
@@ -1,7 +1,7 @@
 # Copyright (C) DevCartel Co.,Ltd.
 # Email: support@devcartel.com
 import sys
-__version__ = '1.2.0'
+__version__ = '1.3.0'
 
 if sys.version_info >= (3, 0):
     from pyflx.pyflx import *
